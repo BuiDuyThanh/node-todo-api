866af9129660d15db72e464c0fa09bd89a7733b6
BuiDuyThanh <buiduythanh6291taikhoan@gmail.com>
2017-07-26 04:35:37 +0300

    Add POST /users route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
-var env = process.env.NODE_ENV || 'development';		// NODE_ENV is set when run on test and production, otherwise development will be set
+var env = process.env.NODE_ENV || 'development';		// NODE_ENV is set when run on test (defined in package.json) and production, otherwise development will be set
 //	console.log('env *****', env);
 
+// heroku is 'production' so it won't pass in the below
 if (env === 'development') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
