e2a915e71e0bc56f43e346bbee7389dd4d1e9bcc
BuiDuyThanh <buiduythanh6291taikhoan@gmail.com>
2017-08-15 03:23:45 +0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,19 @@
 var env = process.env.NODE_ENV || 'development';		// NODE_ENV is set when run on test (defined in package.json) and production, otherwise development will be set
 //	console.log('env *****', env);
 
+if(env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	var envConfig = config[env];
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
+}
+
+/*
+
 // heroku is 'production' so it won't pass in the below
+
 if (env === 'development') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -9,3 +21,5 @@ if (env === 'development') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+
+*/
